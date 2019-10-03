0dd1a3d4a27bfcc28867be232d7412aadc3087c1
calvin goh <calvingoh1347@gmail.com>
2019-10-04 00:44:36 +0800

    Login and logout done

--- app/src/main/java/com/example/kduclubandsociety/AccountActivity.java
+++ app/src/main/java/com/example/kduclubandsociety/AccountActivity.java
@@ -1,6 +1,35 @@
 package com.example.kduclubandsociety;
 
-import android.app.Activity;
+import androidx.appcompat.app.AppCompatActivity;
 
-public class AccountActivity extends Activity {
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class AccountActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private Button signoutbutton;
+
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_account);
+
+        findViewById(R.id.signOutButton).setOnClickListener(this);
+
+    }
+    private void signOut() {
+        mAuth.signOut();
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        signOut();
+    }
 }
--- app/src/main/java/com/example/kduclubandsociety/MainActivity.java
+++ app/src/main/java/com/example/kduclubandsociety/MainActivity.java
@@ -26,11 +26,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private static final String TAG = "EmailPassword";
 
     private TextView mStatusTextView;
-    private TextView mDetailTextView;
     private EditText mEmailField;
     private EditText mPasswordField;
 
     private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
 
 
     @Override
@@ -41,10 +41,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         mAuth = FirebaseAuth.getInstance();
 
 
-//
         // Views
         mStatusTextView = findViewById(R.id.status);
-        mDetailTextView = findViewById(R.id.detail);
         mEmailField = findViewById(R.id.fieldEmail);
         mPasswordField = findViewById(R.id.fieldPassword);
 
@@ -59,9 +57,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onStart() {
         super.onStart();
         FirebaseUser currentUser = mAuth.getCurrentUser();
-        updateUI(currentUser);
+
     }
-/*
+
+
+    /*
     private void createAccount(String email, String password) {
         Log.d(TAG, "createAccount:" + email);
         if (!validateForm()) {
@@ -101,27 +101,19 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()) {
-                            Log.d(TAG, "signinwithemail:success");
-                            FirebaseUser user = mAuth.getCurrentUser();
-                            updateUI(user);
+                            // FirebaseUser user = mAuth.getCurrentUser();
+                           Intent intent = new Intent(MainActivity.this, AccountActivity.class);
+                           startActivity(intent);
                         } else {
-                            Log.w(TAG, "signinwithemail:failure", task.getException());
-                            Toast.makeText(MainActivity.this, "Authentication failed", Toast.LENGTH_LONG).show();
-                            updateUI(null);
+                            Toast.makeText(MainActivity.this, "Email/Password Invalid", Toast.LENGTH_LONG).show();
+                           // updateUI(null);
                         }
-                        if (!task.isSuccessful()) {
-                            mStatusTextView.setText(R.string.auth_failed);
-                        }
-                       // hideProgressDialog();
                     }
                 });
 
     }
 
-    private void signOut() {
-        mAuth.signOut();
-        updateUI(null);
-    }
+
 /*
     private void sendEmailVerification() {
         // Disable button
@@ -179,7 +171,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         return valid;
     }
 
-    private void updateUI(FirebaseUser user) {
+   /* private void updateUI(FirebaseUser user) {
        // hideProgressDialog();
         if (user != null) {
             mStatusTextView.setText(getString(R.string.emailpassword_status_fmt,
@@ -200,18 +192,17 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             findViewById(R.id.signedInButtons).setVisibility(View.GONE);
         }
     }
-
+*/
     @Override
     public void onClick(View v) {
         int i = v.getId();
           if (i == R.id.emailSignInButton) {
             signIn(mEmailField.getText().toString(), mPasswordField.getText().toString());
-        } else if (i == R.id.signOutButton) {
-            signOut();
+        }// else if (i == R.id.signOutButton) {
+            //signOut();
         }
-
     }
-}
+
 
 
 
