3310fe2ff9874f45a1db029e75c18f435aa63f5a
charitylxy <charitylxy@gmail.com>
2019-11-02 16:35:04 +0800

    profile layout able to show the user details (havent done)

--- app/src/main/java/com/example/kduclubandsociety/Profile/ProfileActivity.java
+++ app/src/main/java/com/example/kduclubandsociety/Profile/ProfileActivity.java
@@ -6,14 +6,20 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.kduclubandsociety.R;
 import com.example.kduclubandsociety.Utils.BottomNavigationViewHelper;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -21,21 +27,24 @@ public class ProfileActivity extends AppCompatActivity {
     private static final String TAG = "ProfileActivity";
     private static final int ACTIVITY_NUM = 3;
     private Context mContext = ProfileActivity.this;
-/*
+
     //firebase
     private FirebaseDatabase mFirebaseDatabase;
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener mAuthListener;
     DatabaseReference myRef;
 
- */
+
+    private Button mSignOut;
+    private TextView mName;
+    private TextView mEmail;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
         setupBottomNavigationView();
-/*
+
         Intent intent = getIntent();
         String uid = intent.getStringExtra("currentUid");
 
@@ -43,8 +52,33 @@ public class ProfileActivity extends AppCompatActivity {
         mAuth = FirebaseAuth.getInstance();
         mFirebaseDatabase = FirebaseDatabase.getInstance();
 
- */
+        //initialize views
+        mSignOut = findViewById(R.id.signoutButton);
+        mName = findViewById(R.id.txtName);
+        mEmail = findViewById(R.id.txtEmail);
+
+
+        myRef = FirebaseDatabase.getInstance().getReference().child("Student").child(uid);
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                String name = dataSnapshot.child("name").getValue().toString();
+                String email = dataSnapshot.child("email").getValue().toString();
+
+                mName.setText(name);
+                mEmail.setText (email);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
     }
+
+
 
     // set up bottom navigation bar
     private void setupBottomNavigationView(){
